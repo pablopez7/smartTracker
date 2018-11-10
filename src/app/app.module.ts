6b115221aa30a4ca65347189de4f2c5029d90728
Pablo López <pablopez7@gmail.com>
2018-11-09 22:24:15 -0600

    Angular 7

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/shared/navbar/navbar.component';
+import { SidenavComponent } from './components/shared/sidenav/sidenav.component';
+import { LoginComponent } from './components/auth/login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    SidenavComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
